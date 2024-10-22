e193554e0096b377cae9afd5a6ce037f2afa135f
Viswa Gandamalla <128842289+viswa314@users.noreply.github.com>
2024-10-22 18:47:03 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_St4V37Hj5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7mofa0t0egu0bunn3lr5nqj6o3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
